3975ef45ee8f4ad1831c52916c6b7aa4b8bab8cb
msk- <mattkingston@gmail.com>
2019-11-26 19:16:46 +0000

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const Koa = require('koa');
+
+const app = new Koa();
+
+app.use(async (ctx) => {
+    ctx.body = 'Hello World';
+});
+
+module.exports = {
+    app,
+};
---
+++ src/server.js
@@ -0,0 +1,3 @@
+const { app } = require('./app');
+
+app.listen(3000, console.log('Listening on port 3000...'));

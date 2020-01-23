edeeabfd0ac75243e6f1d15a927f6cbcd1ce2b64
msk- <mattkingston@gmail.com>
2020-01-23 21:20:45 +0000

    Taking db config from environment

--- src/server.js
+++ src/server.js
@@ -3,7 +3,18 @@ const { Logger, transports } = require('el-logger');
 
 const Database = require('./db');
 const reportsConfig = require('../config/reports.json');
-const dbConfig = require('../config/db.json');
+const dbConfig = {
+    connection: {
+        host: process.env['DB_HOST'] || 'localhost',
+        user: process.env['DB_USER'] || 'root',
+        password: process.env['DB_PASSWORD'] || 'root',
+        database: process.env['DB_DATABASE'] || 'central_ledger',
+    },
+    pool: {
+        min: 1,
+        max: 10,
+    },
+};
 
 const db = new Database(dbConfig);
 
